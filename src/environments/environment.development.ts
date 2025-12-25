d12cf06eaeadb0c754415f490d7df41b02bad1c5
Abdelaziz Zahran <zizoelbeltagy266@gmail.com>
2025-12-25 15:51:27 +0200

    add production link

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
-//test
-export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:44361/api/',
-    secretKey: '16171871AhmedSalah@@##', // هذا هو المفتاح السري
-};
-//Production
+// //test
 // export const environment = {
-//     production: true,
-//     apiUrl: 'http://alahramanc-001-site1.rtempurl.com/api/',
-//     secretKey: '16171871AhmedSalah', // هذا هو المفتاح السري
+//     production: false,
+//     apiUrl: 'https://localhost:44361/api/',
+//     secretKey: '16171871AhmedSalah@@##', // هذا هو المفتاح السري
 // };
+//Production
+export const environment = {
+    production: true,
+    apiUrl: 'http://bilqalaam.somee.com/api/',
+    secretKey: 'ADH53476@y77', // هذا هو المفتاح السري
+};
