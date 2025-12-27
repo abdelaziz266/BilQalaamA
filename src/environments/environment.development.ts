fa7598d923d062445ace32d5980f6c6956168c37
abdelaziz266 <abdelaziz266@gmail.com>
2025-12-27 08:12:52 +0200

    env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 // //test
-export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:44361/api/',
-    secretKey: '16171871AhmedSalah@@##', // هذا هو المفتاح السري
-};
-//Production
 // export const environment = {
-//     production: true,
-//     apiUrl: 'https://bilqalaam.somee.com/api/',
-//     secretKey: 'ADH53476@y77', // هذا هو المفتاح السري
+//     production: false,
+//     apiUrl: 'https://localhost:44361/api/',
+//     secretKey: '16171871AhmedSalah@@##', // هذا هو المفتاح السري
 // };
+//Production
+export const environment = {
+    production: true,
+    apiUrl: 'https://bilqalaam.somee.com/api/',
+    secretKey: 'ADH53476@y77', // هذا هو المفتاح السري
+};
